d5efc495b8b143c0522ab3a282dc89f18e716b7d
Ghsjulian <ghsjulian@gmail.com>
2024-07-01 23:10:26 +0600

    Cors Added

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 
+// Enable CORS
+app.use(cors());
+
+// Parse JSON bodies
+app.use(express.json());
+
+
+
 app.get("/", (req, res) => {
-  res.send("Welcome This Is Home Directory !");
+  //res.send("Welcome This Is Home Directory !");
+  const data = {code:200,status:"Success", message: 'This Is Home Route' };
+  res.json(data);
 });
 
 const PORT = process.env.PORT || 5000;
