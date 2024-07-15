6a8d9933955df889a6ab9c83aed457c1af2ceda6
Ghsjulian <ghsjulian@gmail.com>
2024-07-15 22:12:06 +0600

    Skipping...

---
+++ src/controllers/productController.js
@@ -0,0 +1,36 @@
+const Product = require("../models/Products.js");
+const multer = require('multer');
+const myFunction = require("../auth/functions");
+const dotenv = require("dotenv");
+dotenv.config({ path: "../../.env" });
+var apiUrl = process.env.API_URL;
+const upload = multer({ dest: './uploads/' });
+
+
+class Products {
+    async addProduct(req, res) {
+        console.log(req.body);
+        res.json({ok:"okkk"})
+        /*
+        const product_title = req.body.product_title;
+        const product_img = req.body.product_img;
+        const product_desc = req.body.product_desc;
+        const product_price = req.body.product_price;
+        const product_category = req.body.product_category;
+        // Validation
+        if (!product_title || !product_img || !product_desc || !product_price || !product_category) {
+            return res.status(400).json({
+                code: 403,
+                type: false,
+                status: "failed",
+                error: "All Fields Are Required"
+            });
+        }
+        if (product_title && product_img && product_desc && product_price && product_category) {
+        res.json({ok:"Okkk"})
+        }
+        */
+    }
+}
+let product = new Products();
+module.exports = product;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,15 +1,18 @@
 const express = require("express");
-const user = require("../controllers/userController.js");
+const user = require("../controllers/userController");
+const product = require("../controllers/productController");
 const myFunction = require("../auth/functions");
-const isAuth = require("../auth/isAuth")
+const isAuth = require("../auth/isAuth");
 const router = express.Router();
 
+/* Users And Clients Access Routes */
 router.post("/signup", user.register);
 router.post("/login", user.login);
 router.post("/logout", user.logout);
 router.post("/user/verification", user.verifyEmail);
 router.get("/users", user.users);
 router.get("/users/delete/:id", user.deleteUser);
-// router.post("/is-user", userController.isExistUser);
-//
+/* Admin Access Routes */
+router.post("/admin/add-product", product.addProduct);
+
 module.exports = router;
