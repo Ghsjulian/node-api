8fdea30b8673ff28bf1de840b74d855e2a6c7e04
Ghsjulian <ghsjulian@gmail.com>
2024-07-28 19:09:16 +0600

    final code...

--- src/database/db.js
+++ src/database/db.js
@@ -3,22 +3,18 @@ const dotenv = require("dotenv");
 require("dotenv").config();
 dotenv.config({ path: "../../.env" });
 /* LOCALHOST URI */
-const user = "ghsjulian";
-const pwd = "ghsjulian";
-
-//const uri = `mongodb://${user}:${pwd}@localhost:27017`;
+/*
 const uri = `mongodb://localhost:27017/`;
 const PORT = process.env.PORT || 5000;
 const db = process.env.DB_NAME || "shopping-cart";
-
+*/
 /* REMOTE URI */
-/*
+
 const uri = process.env.URI;
 const PORT = process.env.PORT || 5000;
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 const db = process.env.DB_NAME;
-*/
 
 mongoose
     .connect(uri, { dbName: db })
